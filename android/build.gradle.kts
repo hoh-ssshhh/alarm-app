3c8ac47010009553cd8165ee2d681d56848701d5
hoh-ssshhh <hou.oshumi@gmail.com>
2025-05-06 15:01:51 +0800

    your message about changes

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,6 +1,13 @@
+// プロジェクトレベルの build.gradle
+
+plugins {
+    id 'com.android.application'
+    id 'com.google.gms.google-services'  // ← これを追加
+}
+
 allprojects {
     repositories {
-        google()
+        google()  // Googleのリポジトリを追加
         mavenCentral()
     }
 }
@@ -12,6 +19,7 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
@@ -19,3 +27,5 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
+apply plugin: 'com.google.gms.google-services'  // ← これを追加
